54c3b2415c84a518d8ea07d97b6067b1021ede32
LeeMinjae <LMJ0900@github.com>
2024-11-01 23:06:00 +0900

    123

---
+++ workspace/app/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="flex justify-center">
+      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+      </div>
+      <h1>Welcome to this NextJS Course!</h1>
+      <p>🔥 Let&apos;s get started! 🔥</p>
+    </main>
+  );
+}
